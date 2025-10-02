35c0e40695a3082eea95e0c83e57ee811f807260
Kakeru Hokida <me@6mile.dev>
2025-10-02 22:15:14 +0900

    add extension methods for dependency injection and application configuration

---
+++ Ice/Configuration/DependencyInjectionExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace Ice.Configuration;
+
+public static class DependencyInjectionExtension
+{
+    public static IServiceCollection AddIceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        
+        return services;
+    }
+}
---
+++ Ice/Configuration/WebApplicationBuilderExtension.cs
@@ -0,0 +1,36 @@
+﻿using Ice.Db;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ice.Configuration;
+
+public static class WebApplicationBuilderExtension
+{
+    public static WebApplicationBuilder AddIceConfiguration(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddRazorPages();
+        
+        // Add DB configuration
+        builder.AddIceDbConfiguration();
+
+        // Add custom services
+        builder.Services.AddIceServices(builder.Configuration);
+
+        return builder;
+    }
+    
+    private static WebApplicationBuilder AddIceDbConfiguration(this WebApplicationBuilder webApplicationBuilder)
+    {
+        var dbConnectionString = webApplicationBuilder.Configuration.GetConnectionString("DbConnection")
+                                 ?? throw new InvalidOperationException("DB ConnectionString must not be null.");
+
+        webApplicationBuilder.Services.AddDbContext<IceDbContext>(options =>
+            options.UseNpgsql(dbConnectionString)
+        );
+        
+        // Register IIceDbContext to resolve to IceDbContext
+        webApplicationBuilder.Services.AddScoped<IIceDbContext>(provider => provider.GetRequiredService<IceDbContext>());
+
+        return webApplicationBuilder;
+    }
+}
