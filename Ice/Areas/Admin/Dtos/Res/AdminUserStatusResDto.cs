aca0f6a6c8e6acc4631ce1673ea1057173c1e6ad
Kakeru Hokida <me@6mile.dev>
2025-10-13 21:29:33 +0900

    feat: add DTOs for admin user and staff status management

---
+++ Ice/Areas/Admin/Dtos/Res/AdminUserStatusResDto.cs
@@ -0,0 +1,15 @@
+namespace Ice.Areas.Admin.Dtos.Res;
+
+public class AdminUserStatusResDto
+{
+    public long Id { get; init; }
+    public string FullName { get; init; } = string.Empty;
+    public bool IsWorking { get; init; }
+    public IReadOnlyList<CurrentTicketDto> CurrentTickets { get; init; } = [];
+}
+
+public class CurrentTicketDto
+{
+    public long Id { get; init; }
+    public string Title { get; init; } = string.Empty;
+}
